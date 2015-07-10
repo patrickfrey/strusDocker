aad089249cec484c14d411b21ea3bc4d6263b1cd
patrickfrey <patrickpfrey@yahoo.com>
2015-07-10 14:34:23 +0200

    fixed bugs. nginx based container is now running standalone, even if storage does not yet exist

--- strus-ub1404-nginx/entrypoint.sh
+++ strus-ub1404-nginx/entrypoint.sh
@@ -1,39 +1,11 @@
 #!/bin/bash
 set -e
 
-# -- parse argument string and extract some known options:
-arg=$@
-storage=""
-while [[ $# > 0 ]]
-do
-key="$1"
-case $key in
-	-s|--storage)
-	storage="$2"
-	shift
-	;;
-	*)
-	shift
-	;;
-esac
-done
-
 # -- start required services:
 service php5-fpm start
 service nginx start
 
-# -- command handler:
-case "$arg[0]" in
-	strusRpcServer*)
-		if [[ -d "$storage" ]]
-		then echo "Storage $storage exists"
-		else
-			strusCreate -s "path=$storage";
-		fi
-		exec "$arg"
-	;;
-	*)
-		exec "$arg"
-	;;
-esac
+echo "$@"
+exec "$@"
+
 
