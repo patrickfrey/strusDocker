aa7006d06662e1c7203ee84895004bd0c900b315
patrickfrey <patrickpfrey@yahoo.com>
2017-12-20 11:21:55 +0100

    remove image for strus build clang with c++98 (clang does not like boost regex, clang only C++>11)

--- strus-ub1604-clang98/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /strus-master/; make test
-cd /strusAnalyzer-master; make test
-cd /strusModule-master; make test
-cd /strusPattern-master; make test
-cd /strusVector-master; make test
-cd /strusUtilities-master; make test
-cd /strusBindings-master; make test
