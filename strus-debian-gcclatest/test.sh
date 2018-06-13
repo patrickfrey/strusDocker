6166c34541085e1ef03d47d56bd410383ef9b29e
patrickfrey <patrickpfrey@yahoo.com>
2018-06-13 13:40:19 +0200

    docker file for debian build of strus with gcc

---
+++ strus-debian-gcclatest/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /strusBase-master/; make test
+cd /strus-master/; make test
+cd /strusAnalyzer-master; make test
+cd /strusModule-master; make test
+cd /strusPattern-master; make test
+cd /strusVector-master; make test
+cd /strusUtilities-master; make test
+cd /strusBindings-master; make test
+
